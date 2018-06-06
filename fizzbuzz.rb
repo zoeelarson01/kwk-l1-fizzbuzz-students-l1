bc0aaabbf13193693647cb46d6d2968a2d135f83
zoeelarson01 <zoeelarson@gmail.com>
2018-06-06 17:49:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 
 def fizzbuzz(number)
   if #{number} % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif #{number} % 5 == 0 
-    puts "Buzz"
+    "Buzz"
   elsif #{number} % 3 == 0 && #{number} % 5 == 0 
-    puts "Fizzbuzz"
+    "Fizzbuzz"
   else 
-   puts "nil"
+   "nil"
   end
 end
+
 fizzbuzz(15)
