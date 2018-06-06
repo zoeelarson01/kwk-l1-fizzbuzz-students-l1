1593b0473136234061b0660fa80de3ebe4fe0a85
zoeelarson01 <zoeelarson@gmail.com>
2018-06-06 17:53:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,3 +12,14 @@ def fizzbuzz(number)
 end
 
 fizzbuzz(15)
+
+def fizzbuzz
+  if number % 3 == 0 
+    puts "Fizz"
+    elsif number % 5 == 0 
+      puts "Buzz"
+      elsif number % 3 == 0 && % 5 == 0 
+        puts "Fizzbuzz"
+      else 
+        puts "nil"
+      end 
