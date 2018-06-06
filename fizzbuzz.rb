078d68f92f64c4f424a582067fe11c946693bf0a
zoeelarson01 <zoeelarson@gmail.com>
2018-06-06 17:47:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,13 @@ puts "What number would you like?"
 
 number = gets.chomp
 def fizzbuzz(number)
-if #{number} % 3 == 0
-  puts "Fizz"
-elsif #{number} % 5 == 0 
-  puts "Buzz"
-elsif #{number} % 3 == 0 && #{number} % 5 == 0 
-  puts "Fizzbuzz"
-else 
-  puts "nil"
+  if #{number} % 3 == 0
+    puts "Fizz"
+  elsif #{number} % 5 == 0 
+    puts "Buzz"
+  elsif #{number} % 3 == 0 && #{number} % 5 == 0 
+    puts "Fizzbuzz"
+  else 
+   puts "nil"
+  end
 end
